2ca9b85be8b9318728e067b12129608667b3e833
Abhishek <M1050754@mindtree.com>
2020-08-20 18:30:37 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import TodoApp from './components/TodoApp';
 
 const App = () => {
   return (
-    <div className=" container header">
-      <h2 className="container teal-text">TodoList App</h2>
-      <TodoApp />
-    </div>
+    <>
+      <nav>
+        <div className="nav-wrapper teal">
+          <a href="#" className="brand-logo center">
+            Notes App
+          </a>
+        </div>
+      </nav>
+      <div className=" container header">
+        <TodoApp />
+      </div>
+    </>
   );
 };
 
